e831aaf0fc25e3b9fb7093cf5802836fc51d1536
Makz <makz.ru@mail.ru>
2023-11-26 22:22:39 +0400

    Статус: Начальное состояние движка Версия: 1.0
    Описание:
    Это самый голый движок сделанный за минимальное время (мое мнение)
    Однозначно код движка можно улучшить с точки зрения его читабельности
    Добавлен пустой класс Игрок чтобы сражаться армиями созданных игроков
    
    Результат:
    происходит сражение между двумя кораблями
    ходы соперников происходят одновременно (теоретически оба корабля могут потопить друг друга)
    мы как бы наблюдаем за этим процессом (через print)
    свойства класса Корабль взяты случайными величинами для иммитации боя (скорее всего механизм иммитации боя в случае сражения например двух армий и большого числа кораблей будет отличаться для например увеличения производительности)

---
+++ calculations2.py
@@ -0,0 +1,33 @@
+from random import randint
+
+class Ship:
+    def __init__(self):
+        self.damage = randint(1,10)
+        self.health = randint(50,100)
+
+class Player:
+    pass
+
+class Calculations:
+    def __init__(self, ship1: Ship, ship2: Ship):
+        self.ship1 = ship1
+        self.ship2 = ship2
+
+    def calculation(self):
+        while self.ship1.health >= 0 and self.ship2.health >= 0:
+            self.ship1.health = self.ship1.health - self.ship2.damage
+            self.ship2.health = self.ship2.health - self.ship1.damage
+            print(self.ship1.health, self.ship2.health) # проверка кто быстрее погибает по раундам
+
+        print(self.ship1.health, self.ship2.health) # выясняем победителя
+
+def main():
+    ship1 = Ship()
+    ship2 = Ship()
+    calculation = Calculations(ship1, ship2)
+    calculation.calculation()
+
+
+
+if __name__ == "__main__":
+    main()
